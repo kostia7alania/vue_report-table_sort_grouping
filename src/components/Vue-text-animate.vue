e0b985fca18b031182072218ce855dedaa4e2d47
kostia7alania <osetia-alania@mail.ru>
2019-02-14 14:11:58 +0300

    add animated number & text(not realised)

---
+++ src/components/Vue-text-animate.vue
@@ -0,0 +1,32 @@
+<template>
+  
+</template>  
+
+
+ <script>
+
+//недоделанная тема - хотел сделать анимацию меж переходами меж цифрами=)
+export default {
+  props: { word: { type: String, default: () => '' } },
+  data() { return {  }; },
+  mounted() { },
+  watch: {},
+  methods: {  },
+  computed: {  };
+}
+</script> 
+<style>
+  /* Анимации появления и исчезновения могут иметь */
+/* различные продолжительности и динамику.       */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
